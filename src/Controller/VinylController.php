a5f7f628353bed041d272ae06f88d5c17a20e40c
yxj0312 <yanxiajun@hotmail.com>
2023-10-01 21:44:41 +0200

    add wildcard

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController 
 {    
@@ -11,5 +12,16 @@ class VinylController
     public function homepage(): Response
     {
        return new Response('Welcome to the homepage!');
+    }
+
+     #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+        return new Response($title);
     }
 }
