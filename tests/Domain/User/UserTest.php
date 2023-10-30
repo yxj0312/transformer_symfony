39fcc0e81b09bf157e9b4169c80ddbb61d78e5a1
yxj0312 <yanxiajun@hotmail.com>
2023-10-30 22:36:09 +0100

    add usertest

--- tests/Domain/User/UserTest.php
+++ tests/Domain/User/UserTest.php
@@ -2,8 +2,42 @@
 
 namespace App\Tests\Domain\User;
 
+use App\Entity\User;
+
 class UserTest extends \PHPUnit\Framework\TestCase
 {
+    public function testGetName()
+    {
+        $user = new User();
+        $user->setName('John Doe');
+        
+        $this->assertEquals('John Doe', $user->getName());
+    }
     
+    public function testSetName()
+    {
+        $user = new User();
+        $user->setName('John Doe');
+        
+        $this->assertEquals('John Doe', $user->getName());
+    }
+    
+    public function testGetCreatedAt()
+    {
+        $user = new User();
+        $now = new \DateTime();
+        $user->setCreatedAt($now);
+        
+        $this->assertEquals($now, $user->getCreatedAt());
+    }
+    
+    public function testSetCreatedAt()
+    {
+        $user = new User();
+        $now = new \DateTime();
+        $user->setCreatedAt($now);
+        
+        $this->assertEquals($now, $user->getCreatedAt());
+    }
 
 }
