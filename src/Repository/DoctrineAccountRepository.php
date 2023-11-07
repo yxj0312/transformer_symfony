da9348af9d2ced0d81b052d3305b508c6cdb9599
yxj0312 <yanxiajun@hotmail.com>
2023-11-07 22:06:58 +0100

    fullfil doctrineAccountRepo

--- src/Repository/DoctrineAccountRepository.php
+++ src/Repository/DoctrineAccountRepository.php
@@ -5,29 +5,52 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Domain\Account\Contract\AccountRepositoryInterface;
-use App\Entity\User;
+use App\Entity\Account;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Domain\Account\Model\Account;
+use App\Domain\Account\Contract\AccountRepositoryInterface;
+use App\Entity\Account;
 use Doctrine\ORM\EntityManagerInterface;
 
 final class DoctrineAccountRepository implements AccountRepositoryInterface
 {
-    /**
-     * @var EntityRepository<Account>
-     */
-    private $repository;
-
     /**
      * @var EntityManagerInterface 
      */
     private $entityManager;
 
     /**
-     * UserRepository constructor.
+     * @var EntityRepository<Account>
+     */
+    private $repository;
+
+    /**
+     * DoctrineAccountRepository constructor.
      *
      * @param EntityManagerInterface $entityManager
      */
     public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->repository = $entityManager->getRepository(Account::class);
         $this->entityManager = $entityManager;
+        $this->repository = $entityManager->getRepository(Account::class);
     }
+
+    /**
+     * @inheritDoc
+     */
+    public function findByEmail(string $email): ?Account
+    {
+        return $this->repository->findOneBy(['email' => $email]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function findById(int $id): ?Account
+    {
+        return $this->repository->find($id);
+    }
+
+    
 }
+
