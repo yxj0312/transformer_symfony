0f376e5ae0f3ee13bef4b67bce4b6d4587afb604
yxj0312 <yanxiajun@hotmail.com>
2023-11-08 21:53:12 +0100

    add user service

---
+++ src/Service/UserService.php
@@ -0,0 +1,20 @@
+namespace App\Service;
+
+   use App\Repository\UserRepository;
+
+   class UserService
+   {
+       private $userRepository;
+
+       public function __construct(UserRepository $userRepository)
+       {
+           $this->userRepository = $userRepository;
+       }
+
+       public function createUser($userData)
+       {
+           // Logic to create a user
+       }
+
+       // Other user-related operations
+   }
