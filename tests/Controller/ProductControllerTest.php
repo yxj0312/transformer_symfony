019855daa63a7217291f35514b859f143bc018d6
yxj0312 <yanxiajun@hotmail.com>
2023-12-13 22:30:49 +0100

    remove test

--- tests/Controller/ProductControllerTest.php
+++ tests/Controller/ProductControllerTest.php
@@ -2,13 +2,13 @@
 
 namespace App\Test\Controller;
 
-use App\Entity\Product;
-use App\Repository\ProductRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProductControllerTest extends WebTestCase
 {
-    
+    /** @test */
+    function testIndex()
+    {
+        
+    }
 }
