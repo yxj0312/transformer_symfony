adfeeb00e6c82169d51f17ba9c5db34146b38da1
yxj0312 <yanxiajun@hotmail.com>
2024-01-31 22:13:01 +0100

    update test

--- tests/Domain/User/UserTest.php
+++ tests/Domain/User/UserTest.php
@@ -10,34 +10,25 @@ class UserTest extends \PHPUnit\Framework\TestCase
     {
         $user = new User();
         $user->setName('John Doe');
-        
+
         $this->assertEquals('John Doe', $user->getName());
     }
-    
+
     public function testSetName()
     {
         $user = new User();
         $user->setName('John Doe');
-        
+
         $this->assertEquals('John Doe', $user->getName());
     }
-    
-    public function testGetCreatedAt()
-    {
-        $user = new User();
-        $now = new \DateTime();
-        $user->setCreatedAt($now);
-        
-        $this->assertEquals($now, $user->getCreatedAt());
-    }
-    
-    public function testSetCreatedAt()
-    {
-        $user = new User();
-        $now = new \DateTime();
-        $user->setCreatedAt($now);
-        
-        $this->assertEquals($now, $user->getCreatedAt());
-    }
 
+    public function testCreatedAt()
+    {
+        $user = new User();
+        $now = new \DateTimeImmutable(); // Use DateTimeImmutable
+
+        // Test setCreatedAt and getCreatedAt together
+        $user->setCreatedAt($now);
+        $this->assertEquals($now, $user->getCreatedAt());
+    }
 }
