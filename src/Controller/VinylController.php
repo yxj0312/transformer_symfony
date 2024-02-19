a70fc2103a5a6ad023f3c4b522527f2844bc25f8
yxj0312 <yanxiajun@hotmail.com>
2024-02-19 23:07:34 +0100

    fix

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -9,27 +9,5 @@ use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {    
-    #[Route('/', 'app_homepage')]
-    public function homepage(): Response
-    {
-        die();
-        dd('hello');
-       return $this->render('vinyl/homepage.html.twig'
-       , [
-              'title' => 'Vinyl Record Database',
-              'genres' => ['Rock', 'Pop', 'Jazz', 'Classical', 'Other']
-         ]
-       );
-    }
-
-    #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(string $slug = null): Response
-    {
-        if ($slug) {
-            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
-        } else {
-            $title = 'All Genres';
-        }
-        return new Response($title);
-    }
+    
 }
